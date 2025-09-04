c33ab53629a1c0c9b00ead6b49a88a29b14650e4
WeiJun <wong.weijun923@gmail.com>
2025-09-04 15:00:34 +0800

    added buttons

--- tkinter/tutorial.py
+++ tkinter/tutorial.py
@@ -1,11 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first")
 window.minsize(width=100,height=300)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "italic", "bold"))
+my_label = Label(text="I am a label", font=("Arial", 24, "italic", "bold"))
 my_label.pack(side="left", expand=True)
+
+my_label["text"] = "I am a label"
+my_label.config(text="I am a label")
+
+def button_clicked():
+    new_text = input.get()
+    print("I got clicked")
+    my_label.config(text=new_text)
+
+button = Button(window, text="Click me", command = button_clicked)
+button.pack(side="right")
+
+input = Entry()
+input.pack(side="left")
 
 
 window.mainloop()
