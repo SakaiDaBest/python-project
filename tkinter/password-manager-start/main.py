698c358f7cc0efe6e4cc7e8241160f8d88e3d57c
WeiJun <wong.weijun923@gmail.com>
2025-09-07 18:45:53 +0800

    edited the UI

--- tkinter/password-manager-start/main.py
+++ tkinter/password-manager-start/main.py
@@ -1,5 +1,21 @@
+from tkinter import *
+from PIL import ImageTk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas = Canvas(window, width=200, height=200)
+logo = ImageTk.PhotoImage(file="logo.png")
+canvas.create_image(100,100, image=logo)
+canvas.grid(row=0, column=1)
+
+
+
+
+
+window.mainloop()
